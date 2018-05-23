2dec428034038540e6146044da245e3c91f66bec
KelseyJ22 <kelsey2@stanford.edu>
2018-05-23 14:07:42 -0700

    made changes to match discussion

--- rev1_full_sim.py
+++ rev1_full_sim.py
@@ -6,55 +6,70 @@ from time import sleep
 class Farm:
 	def __init__(self):
 		self.window = GraphWin('Farm', 500, 500)
+		self.mono = False
 		self.pesticide = False
 		self.fertilizer = False
 		self.money = 0
-		self.pond_alive = True
-		self.algae = 1
-		self.field_alive = True
+		self.pond_health = 100
+		self.algae_coverage = 1
+		self.field_health = 100
 		self.buttons = dict()
 		self.window.autoflush = False
+		self.buttons['mono_poly'] = [Point(325, 250), Point(500, 300)]
 		self.buttons['pesticide'] = [Point(325, 300), Point(500, 350)]
 		self.buttons['fertilizer'] = [Point(325, 350), Point(500, 400)]
 		self.buttons['GO'] = [Point(325, 400), Point(500, 450)]
 
 
 	def log_state(self):
-		return 'pesticides applied: ' + str(self.pesticide) + '\nfertilizer applied: ' + str(self.fertilizer) + '\nmoney: $' + str(self.money) + '\npond_alive: ' + str(self.pond_alive) + '\nfield_alive: ' + str(self.field_alive) + '\nalgae: ' + str(self.algae)
+		return 'FARM STATE \nPesticides applied: ' + str(self.pesticide) + '\nFertilizer applied: ' + str(self.fertilizer) + '\nMoney: $' + str(self.money) + '\nPond health: ' + str(self.pond_health) + '\nField health: ' + str(self.field_health) + '\nAlgae coverage: ' + str(self.algae_coverage)
 
 
 	def run_round(self):
 		self.display()
 		self.make_choices()
 
-		if self.pesticide:
-			self.money += 5
-			self.algae *= 2 # exponential
+		if self.mono:
+			self.money += 200
+			self.field_health -= 50
 		else:
-			self.money += 1
+			self.money += 50
+			self.field_health += 10
+
+
+		if self.pesticide:
+			self.money += 50
+			self.field_health -= 20
+			self.algae_coverage *= 2 # exponential
+
 
 		if self.fertilizer:
-			self.money += 10
-			self.algae *= 2 # exponential
-		else:
-			self.money += 5
-		if self.algae > 5:
-			self.pond = False
+			self.money += 50
+			self.field_health += 20
+			self.algae_coverage *= 2 # exponential
+
+
+		if self.algae_coverage > 1:
+			self.pond_health -= self.algae_coverage
 
 		# field surviving is more likely if you use chemicals
-		if self.fertilizer:
+		"""if self.fertilizer:
 			if self.pesticide:
-				rand = 10
+				self.field_health -= 50
+				rand = 20
 			else:
-				rand = 6
+				rand = 15
 		else:
-			rand = 4
+			rand = 10
 
 		if random.randint(0, rand) == 0:
-			self.field_alive = False
+			self.field_health = 0
+			print('your field was wiped out by a weather event!')"""
+
+		if self.field_health == 0:
+			self.money -= 300
 
 		print(self.log_state())
-
 
 
 	def in_button(self, loc, button):
@@ -76,6 +91,12 @@ class Farm:
 				self.fertilizer = False
 			else:
 				self.fertilizer = True
+		elif self.in_button(click, self.buttons['mono_poly']):
+			if self.mono:
+				self.mono = False
+			else:
+				self.mono = True
+
 		self.refresh_buttons()
 
 
@@ -92,24 +113,32 @@ class Farm:
 
 
 	def refresh_buttons(self):
+		mono_poly = Rectangle(self.buttons['mono_poly'][0], self.buttons['mono_poly'][1])
+		mono_poly.setFill('white')
+		if self.mono:
+			mono_poly_label = Text(Point(412, 275), 'Switch to Polyculture')
+		else:
+			mono_poly_label = Text(Point(412, 275), 'Switch to Monoculture')
+		mono_poly.draw(self.window)
+		mono_poly_label.draw(self.window)
+
+
 		pesticide = Rectangle(self.buttons['pesticide'][0], self.buttons['pesticide'][1])
 		pesticide.setFill('white')
 		if self.pesticide:
-			pesticide.setOutline('red')
+			pesticide_label = Text(Point(412, 325), 'Do Not Apply Pesticides')
 		else:
-			pesticide.setOutline('black')
+			pesticide_label = Text(Point(412, 325), 'Apply Pesticides')
 		pesticide.draw(self.window)
-		pesticide_label = Text(Point(412, 325), 'pesticides on/off')
 		pesticide_label.draw(self.window)
 
 		fertilizer = Rectangle(self.buttons['fertilizer'][0], self.buttons['fertilizer'][1])
 		fertilizer.setFill('white')
 		if self.fertilizer:
-			fertilizer.setOutline('red')
+			fertilizer_label = Text(Point(412, 375), 'Do Not Apply Fertilizer')
 		else:
-			fertilizer.setOutline('black')
+			fertilizer_label = Text(Point(412, 375), 'Apply Fertilizer')
 		fertilizer.draw(self.window)
-		fertilizer_label = Text(Point(412, 375), 'fertilizer on/off')
 		fertilizer_label.draw(self.window)
 
 		go = Rectangle(self.buttons['GO'][0], self.buttons['GO'][1])
@@ -124,54 +153,56 @@ class Farm:
 		self.refresh_buttons()
 
 		pond = Circle(Point(150, 125), 100)
-		if self.pond_alive:
+		if self.pond_health > 0:
 			pond.setFill('blue')
 		else:
 			pond.setFill('red')
 		pond.draw(self.window)
 
 		field = Rectangle(Point(25, 250), Point(275, 490))
-		if self.field_alive:
+		if self.field_health > 20:
 			field.setFill('green')
 		else:
 			field.setFill('brown')
 		field.draw(self.window)
 
-		clear = Rectangle(Point(300, 5), Point(500, 100))
+		clear = Rectangle(Point(300, 5), Point(500, 150))
 		clear.setFill('white')
 		clear.draw(self.window)
-		results = Text(Point(400, 50), farm.log_state())
+		results = Text(Point(400, 75), farm.log_state())
 		results.draw(self.window)
 
 
 def conclusion(farm, win):
-	window = GraphWin('Conclusion', 500, 500)
+	background = Rectangle(Point(0, 0), Point(500, 500))
+	background.setFill('white')
+	background.draw(farm.window)
 	if win:
 		label = Text(Point(250, 50), 'YOU WIN!')
 	else:
 		label = Text(Point(250, 50), 'YOU LOSE!')
 
-	label.draw(window)
+	label.draw(farm.window)
 
 	results = Text(Point(100, 100), farm.log_state())
-	results.draw(window)
+	results.draw(farm.window)
 
 	message = Text(Point(400, 400), 'Click anywhere to quit.')
-	message.draw(window)
+	message.draw(farm.window)
 
-	window.getMouse()
-	window.close()
+	farm.window.getMouse()
+	farm.window.close()
 
 
 farm = Farm()
 for i in range(0, 5):
 	print('ROUND', i)
 	farm.run_round()
-	if not farm.field_alive:
+	if farm.field_health <= 0 and farm.money <= 300:
 		conclusion(farm, win=False)
 		break
 
-if farm.pond_alive and farm.money > 10 and farm.field_alive:
+if farm.pond_health > 0 and farm.money > 0 and farm.field_health > 0:
 	conclusion(farm, win=True)
 else:
 	conclusion(farm, win=False)
